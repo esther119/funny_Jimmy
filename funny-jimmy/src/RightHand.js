b173b93e20d25ed0decd4a3dd8ff7a1b6bd6ca82
swimmingcircle119 <lampmaa22@gmail.com>
2023-07-23 13:58:40 -0700

    add svgs

---
+++ funny-jimmy/src/RightHand.js
@@ -0,0 +1,12 @@
+// Import the html-react-parser package
+const React = require('react');
+const htmlReactParser = require('html-react-parser');
+
+// Your SVG code
+const svgCode = '<svg width="400" height="110"><rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" /></svg>';
+
+// Convert the SVG code to JSX
+const svgComponent = htmlReactParser(svgCode);
+
+// Export the JSX component so you can use it in other files
+module.exports = svgComponent;
