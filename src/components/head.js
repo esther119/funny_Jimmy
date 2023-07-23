25c8459c1f9bd28af8af618ee92a2cf344336be7
swimmingcircle119 <lampmaa22@gmail.com>
2023-07-23 16:18:35 -0700

    fix the guess how much!

--- src/components/head.js
+++ src/components/head.js
@@ -2,31 +2,17 @@ import React, { useRef } from 'react';
 import audio_file from '../sound/guess_how_much.mp3'
 
 const Head = (props) => {
-    const svgRef = useRef(null);
-
   const handleClick = () => {
     const sound = new Audio(audio_file);
     sound.play();
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <svg
-        ref={svgRef}
+    <div>         
+        <img src={props.image} 
         onClick={handleClick}
-        // className="cursor-pointer w-40 h-40 p-2 bg-gray-300 rounded-full"
-        // xmlns="http://www.w3.org/2000/svg"
-        // viewBox="0 0 24 24"
-        // fill="none"
-        // stroke="currentColor"
-        // strokeWidth="2"
-        // strokeLinecap="round"
-        // strokeLinejoin="round"
-        src={props.image}
-      >
-         
-        {/* <image onClick={handleClick} alt='whatever' src={props.image}  width="40" height="40" /> */}
-      </svg>
+        style={{ cursor: 'pointer' }}
+        />
     </div>
   );
 };
