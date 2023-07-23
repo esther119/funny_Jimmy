ae5503be5f946c28a7b84319fc10b911969f0cc6
Trang Doan <doantrang982@uni.minerva.edu>
2023-07-23 18:14:07 -0400

    refactor the code

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import '../styles/App.css';
+import Head from './head';
+
+const LeftHand = require('./LeftHand');
+const RightHand = require('./RightHand');
+const LeftLeg = require('./LeftLeg');
+const RightLeg = require('./RightLeg');
+// TODO turn into a component
+// const head = require('./components/head');
+const body = require('./body');
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My SVG Example</h1>
+      {LeftHand}
+      {RightHand}
+      {LeftLeg}
+      {RightLeg}
+      <Head/>
+      {body}
+    </div>
+  );
+}
+
+export default App;
--- src/components/head.js
+++ src/components/head.js
@@ -14,14 +14,14 @@ const Head = () => {
       <svg
         ref={svgRef}
         onClick={handleClick}
-        className="cursor-pointer w-40 h-40 p-2 bg-gray-300 rounded-full"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        // className="cursor-pointer w-40 h-40 p-2 bg-gray-300 rounded-full"
+        // xmlns="http://www.w3.org/2000/svg"
+        // viewBox="0 0 24 24"
+        // fill="none"
+        // stroke="currentColor"
+        // strokeWidth="2"
+        // strokeLinecap="round"
+        // strokeLinejoin="round"
       >
         {/* Replace 'myImage.svg' with the path to your SVG image */}
         <image href="public/logo192.png" width="40" height="40" />
